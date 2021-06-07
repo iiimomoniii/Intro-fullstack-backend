8742d9e8fa42eb6d7237361f54669152181fd163
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-07 18:22:51 +0700

    create function for upload image

---
+++ config/multer_config.js
@@ -0,0 +1,38 @@
+const multer = require('multer')
+const fs = require('fs')
+
+module.exports = multerConfig = {
+    config: {
+        //
+        storage: multer.diskStorage({
+            //destination path for paste image files
+            destination: (req, file, next) => {
+                //check if images exists
+                const folder = './images/';
+                //if images not exists
+                if (!fs.existsSync(folder)){
+                    //create images folder
+                    fs.mkdirSync(folder)
+                }
+                //else images exists
+                next(null, folder)
+            },
+            //create filename of image
+            filename: (req, file, next) => {
+                next(null, `${file.fieldname}-${Date.now()}` )
+            }
+        }),
+        //limit file size
+        limits: {fileSize : 1024 * 1024 * 5},
+        //filter upload only image
+        fileFilter: (req, file, next) => {
+            const image = file.mimetype.startsWith('image/')
+            if(image){
+                next(null, true)
+            } else {
+                next({message : 'File type not supported'}, false)
+            }
+        }
+    },
+    keyUpload: 'photo'
+}
--- server.js
+++ server.js
@@ -1,51 +1,15 @@
 const express = require('express');
 const app = express();
 
-//middle ware (Content-Type => application/json)
-app.use(express.json())
-//middle ware (Content-Type => application/x-www-form-urlencoded)
-app.use(express.urlencoded({ extended: false })) //extended: true => nested data structure => {xxx : {yyy : zzz}}
-//middle ware (Content-type => multipart/form-data)
-var multer = require('multer')
-var upload = multer()
-
-//post method (params)
-//http://localhost:1150/product/1
-//result
-//Product id is : 1
-app.post('/product/:id',(req, res) => {
-    res.send(`Product id is : ${req.params.id}`);
-})
-
-//post method (application/json)
-//postman CMS_NodeJS http://localhost:1150/productJSON/1
-//result
-//{ name: 'macbook', price: 10, stock: 999 }
-app.post('/productJSON/:id',(req, res) => {
-    console.log(req.body);
-})
-
-//post method (application/x-www-form-urlencoded)
-//postman CMS_NodeJS http://localhost:1150/productFormUrl/1
-//result
-//[Object: null prototype] {
-//    name: 'macbook',
-//    price: '999',
-//    stock: '777'
-// }
-app.post('/productFormUrl/:id',(req, res) => {
-    console.log(req.body);
-})
+const multer = require('multer')
+//import config
+const multerConfig = require('./config/multer_config')
+//accept keyUpload (photo)
+var upload = multer(multerConfig.config).single(multerConfig.keyUpload)
 
 //post method (multipart/form-data)
-//postman CMS_NodeJS http://localhost:1150/productMutipart/1
-//result
-//[Object: null prototype] {
-//     name: 'macbook',
-//     price: '1234',
-//     stock: '9874'
-//   }
-app.post('/productMutipart/:id',upload.none(),(req, res) => {
+//postman CMS_NodeJS http://localhost:1150/productUploadImage/1
+app.post('/productUploadImage/:id', upload ,(req, res) => {
     console.log(req.body);
 })
 
