161bb6e14c03f3e1ba836f774474f8c89fbaf044
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-08 09:54:29 +0700

    add function forcheck key photo is not correct

--- server.js
+++ server.js
@@ -9,8 +9,21 @@ var upload = multer(multerConfig.config).single(multerConfig.keyUpload)
 
 //post method (multipart/form-data)
 //postman CMS_NodeJS http://localhost:1150/productUploadImage/1
-app.post('/productUploadImage/:id', upload ,(req, res) => {
-    console.log(req.body);
+app.post('/productUploadImage/:id' ,(req, res) => {
+
+    upload(req, res, (err)=> {
+        //condition 
+        if(err instanceof multer.MulterError){
+            console.log(`error : ${JSON.stringify(err)}`);
+        } else if (err) {
+            console.log(`error : ${JSON.stringify(err)}`);
+        }
+        //condition for check filename 
+        const fileName = req.file ? req.file.fieldname : undefined;
+        res.send(`POST Product: ${req.params.id}, ${fileName}`);
+    })
+   
+
 })
 
 
