8b7eab05611f17265185a381702fd2ff7a9ba641
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-08 15:22:13 +0700

    create function for ecut product

--- controller/product_controller.js
+++ controller/product_controller.js
@@ -31,8 +31,8 @@ router.get('/product', async (req, res) => {
 });
 
 //get method with param
-//postman CMS_NodeJS http://localhost:1150/product/1
-router.get('/product/:id', async (req, res) => {
+//postman CMS_NodeJS http://localhost:1150/findbyproductid/1
+router.get('/findbyproductid/:id', async (req, res) => {
     try {
         const result = await db.Products.findOne({
             where: {
@@ -50,20 +50,85 @@ router.get('/product/:id', async (req, res) => {
 });
 
 //post method (multipart/form-data)
-//postman CMS_NodeJS http://localhost:1150/productUploadImage/1
-router.post('/productUploadImage/:id', (req, res) => {
+//postman CMS_NodeJS http://localhost:1150/addproduct
+router.post('/addproduct', (req, res) => {
 
-    upload(req, res, (err) => {
+    upload(req, res, async (err) => {
         //condition 
         if (err instanceof multer.MulterError) {
             console.log(`error : ${JSON.stringify(err)}`);
+            return res.status(500).json({ message : err });
         } else if (err) {
             console.log(`error : ${JSON.stringify(err)}`);
+            return res.status(500).json({ message : err });
         }
-        //condition for check filename 
-        const fileName = req.file ? req.file.fieldname : undefined;
-        res.send(`POST Product: ${req.params.id}, ${fileName}`);
+        //assign data from req to dataobj
+        const data = {
+            ...req.body,
+            image : req.file ? req.file.filename : undefined
+        }
+
+        try {
+            const product = await db.Products.create(data);
+            res.status(201).json(product);
+        } catch (error) {
+            res.status(500).json({ message : error.message });
+        }
     })
 })
+
+//put method (multipart/form-data)
+//postman CMS_NodeJS http://localhost:1150/editproduct/23
+router.put('/editproduct/:id', async (req, res) => {
+    try {
+        const result = await db.Products.findOne({
+            where: {
+                'id': req.params.id
+            }
+        });
+        if(!result){
+            return res.status(404).json({ message : 'Product not found' })
+        } else {
+            updateProduct(req, res, result);
+        }
+    } catch (error) {
+        res.status(500).json({ message : error.message });
+    }
+})
+
+//updateProduct
+function updateProduct (req, res, product) {
+    upload(req, res, async (err)=> {
+        if (err instanceof multer.MulterError) {
+            console.log(`error : ${JSON.stringify(err)}`);
+            return res.status(500).json({ message : err });
+        } else if (err) {
+            console.log(`error : ${JSON.stringify(err)}`);
+            return res.status(500).json({ message : err });
+        }
+
+        const data = {
+            ...req.body,
+            image : req.file ? req.file.filename : undefined
+        }
+        try {
+            //[updated] is array of product object
+            const [updated] = await db.Products.update(data, {
+                where : {
+                    id : product.id
+                }
+            })
+            //if update success 
+            if(updated){
+                const updateProduct = await db.Products.findByPk(product.id)
+                res.status(200).json(updateProduct)
+            }else{
+                throw new Error('Product not found');
+            }
+        } catch (error) {
+            res.status(500).json({ message : error.message })
+        }
+    })
+}
 
 module.exports = router
