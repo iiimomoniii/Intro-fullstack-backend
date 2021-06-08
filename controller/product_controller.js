b8a7a8e5dd52153401d0af870e99a990cc7cd07d
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2021-06-08 15:27:37 +0700

    create function for delete product

--- controller/product_controller.js
+++ controller/product_controller.js
@@ -131,4 +131,23 @@ function updateProduct (req, res, product) {
     })
 }
 
+//delete method (multipart/form-data)
+//postman CMS_NodeJS http://localhost:1150/editproduct/23
+router.delete('/deleteproduct/:id', async (req, res) => {
+    try {
+        const deleted = await db.Products.destroy({
+            where: {
+                'id': req.params.id
+            }
+        });
+        if(!deleted){
+            return res.status(404).json({ message : 'Product not found' });
+        } else {
+            return res.status(204).json({ message : 'Product is deleted' });
+        }
+    } catch (error) {
+        res.status(500).json({ message : error.message });
+    }
+})
+
 module.exports = router
